6e8c514c27257ad8bf5275445cae8f71104f1fa4
zainabbehzad <168668702+zainabbehzad@users.noreply.github.com>
2024-08-30 12:42:16 +0430

    Fixed the vercel errors

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -21,35 +21,35 @@ const reducer = (state = initialState, action) => {
     case INCREMENT_SESSION:
       return state.sessionLength < 60
         ? {
-            ...state,
-            sessionLength: state.sessionLength + 1,
-            timeLeft: (state.sessionLength + 1) * 60,
-          }
+          ...state,
+          sessionLength: state.sessionLength + 1,
+          timeLeft: (state.sessionLength + 1) * 60,
+        }
         : state;
 
     case DECREMENT_SESSION:
       return state.sessionLength > 1
         ? {
-            ...state,
-            sessionLength: state.sessionLength - 1,
-            timeLeft: (state.sessionLength - 1) * 60,
-          }
+          ...state,
+          sessionLength: state.sessionLength - 1,
+          timeLeft: (state.sessionLength - 1) * 60,
+        }
         : state;
 
     case INCREMENT_BREAK:
       return state.breakLength < 60
         ? {
-            ...state,
-            breakLength: state.breakLength + 1,
-          }
+          ...state,
+          breakLength: state.breakLength + 1,
+        }
         : state;
 
     case DECREMENT_BREAK:
       return state.breakLength > 1
         ? {
-            ...state,
-            breakLength: state.breakLength - 1,
-          }
+          ...state,
+          breakLength: state.breakLength - 1,
+        }
         : state;
 
     case RESET: {
