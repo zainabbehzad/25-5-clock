509aaa41d09e4b8bbc15ef0530e1bf4801493512
zainabbehzad <168668702+zainabbehzad@users.noreply.github.com>
2024-08-30 10:27:30 +0430

    Added the Display.js file

---
+++ src/components/Display.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Timer from './Timer';
+import Controls from './Controls';
+
+
+const Display = () => (
+  <div className="display-container">
+    <Timer />
+    <Controls />
+  </div>
+);
+
+export default Display;
